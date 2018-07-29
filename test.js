ac7c1654054dbf3d3d7ffbca13caca9f113f1148
Victor Felder <victor@draft.li>
2018-07-29 21:19:00 +0200

    Fix #3: warn on empty last section

--- test.js
+++ test.js
@@ -10,6 +10,24 @@ const empty = `# A
 ## B (this section is empty!)
 
 ## C
+
+This one is not.
+`;
+
+const emptySectionAsLastNode = `# A
+
+## B
+
+It should warn when the last section is empty!
+
+## C (this section is empty!)
+`;
+
+const twoEmptySections = `# A
+
+## B (empty!)
+
+## C (empty and last!)
 `;
 
 const higher = `# A
@@ -17,6 +35,8 @@ const higher = `# A
 ## B (this section is empty!)
 
 # C
+
+This one is not.
 `;
 
 const lower = `# A
@@ -24,11 +44,15 @@ const lower = `# A
 ## B
 
 ### C
+
+Not empty.
 `;
 
 const ok = `# A
 
 ## C
+
+Not empty.
 `;
 
 test('remark-lint-no-empty-sections', (t) => {
@@ -36,6 +60,21 @@ test('remark-lint-no-empty-sections', (t) => {
     processor.processSync(empty).messages.map(String),
     ['3:30-5:1: Remove empty section: "B (this section is empty!)"'],
     'should warn for empty sections'
+  );
+
+  t.deepEqual(
+    processor.processSync(emptySectionAsLastNode).messages.map(String),
+    ['7:1-7:30: Remove empty section: "C (this section is empty!)"'],
+    'should warn on empty section when it is the last section'
+  );
+
+  t.deepEqual(
+    processor.processSync(twoEmptySections).messages.map(String),
+    [
+      '3:14-5:1: Remove empty section: "B (empty!)"',
+      '5:1-5:23: Remove empty section: "C (empty and last!)"'
+    ],
+    'should warn on all empty sections, including last'
   );
 
   t.deepEqual(
